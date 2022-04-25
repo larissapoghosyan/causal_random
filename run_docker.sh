846f64e5fddd9842b053fb1d6a439114914135b1
cryptexis <gfoxvh@gmail.com>
2022-04-25 02:36:55 +0200

    windows compatible

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/work -e GRANT_SUDO=yes --user root cryptexis/causal-exercise
+docker run --rm -p 8888:8888 -v ${PWD}:/home/jovyan/work -e GRANT_SUDO=yes --user root cryptexis/causal-exercise
